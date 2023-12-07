4cec40cd5e974470d62802ebcf28e3bae1343494
pratiush776 <pratiush776@gmail.com>
2023-12-06 22:41:42 -0500

    finall

--- js/notes.js
+++ js/notes.js
@@ -1,22 +1,22 @@
 const Default =
       "";
-
+console.log(localStorage)
 if (
-    localStorage.getItem("P") === "" ||
-    localStorage.getItem("P") === null ||
-    localStorage.getItem("P") === undefined
+    localStorage.getItem("note") === "" ||
+    localStorage.getItem("note") === null ||
+    localStorage.getItem("note") === undefined
 ) {
-    localStorage.setItem("P", Default);
+    localStorage.setItem("note", Default);
 } else {
     let currentValue = document.getElementById("txt");
-    currentValue.value = localStorage.getItem("P");
+    currentValue.value = localStorage.getItem("note");
 }
 function save() {
     let txt = document.getElementById("txt").value;
-    localStorage.setItem("P", txt);
+    localStorage.setItem("note", txt);
 }
 
 window.onbeforeunload = function () {
     let txt = document.getElementById("txt").value;
-    localStorage.setItem("P", txt);
+    localStorage.setItem("note", txt);
 };
